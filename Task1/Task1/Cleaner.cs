8ab569758578387485ba16cd959c1df3912ae9f9
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-05-10 22:11:05 +0300

    Задание №1

---
+++ Task1/Task1/Cleaner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Cleaner
+    {
+        public DateTime LastAccessTimeUtc { get; set; }
+
+        public Cleaner(string path)
+        {
+            LastAccessTimeUtc = Directory.GetLastAccessTimeUtc(path);
+        }
+
+        public void CleanFolder(string path)
+        {
+
+            if (Directory.Exists(path) && Directory.GetLastAccessTime(path).Minute > TimeSpan.FromMinutes(30).Minutes)
+            {
+                try
+                {
+                    DirectoryInfo directories = new DirectoryInfo(path);
+                    DirectoryInfo[] dirs = directories.GetDirectories();
+                    FileInfo[] files = directories.GetFiles();
+
+                    foreach (var f in files)
+                    { 
+                        f.Delete();
+                        Console.WriteLine($"Файл {f.FullName} был удален");
+
+                    }
+
+                    foreach (var dir in dirs)
+                    {
+                        CleanFolder(dir.FullName);
+                        dir.Delete(true);
+                        Console.WriteLine($"Папка: {dir.FullName} была удалена");
+                    }
+                     
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Произошла ошибка: {error.Message}");
+
+                }
+            
+            
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "set your path to directory";
+            Cleaner clean = new Cleaner(path);
+            clean.CleanFolder(path);
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
