d847b8bda5df3a0f69a20f26590c60c14f66b6fc
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-05-10 23:41:20 +0300

    Задание №2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "your path to directory";
+            Sizer sizer = new Sizer();
+            long lengthFolder = sizer.SizerFolder(path);
+            Console.WriteLine($"Итоговое количество элементов в папке {path}: {lengthFolder}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task2/Task2/Sizer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Sizer
+    {
+        
+
+
+        public long SizerFolder(string path)
+        {
+            long size = 0;
+            while (true)
+            {
+
+                if (Directory.Exists(path))
+                {
+
+                    DirectoryInfo directory = new DirectoryInfo(path);
+                    DirectoryInfo[] dirs = directory.GetDirectories();
+                    FileInfo[] files = directory.GetFiles();
+
+                    try
+                    {
+                        foreach (var file in files)
+                        {
+                            size += file.Length;
+
+                        }
+
+                        foreach (var dir in dirs)
+                        {
+                            size += SizerFolder(dir.FullName);
+
+                        }
+                        
+
+
+                    }
+                    catch (Exception error)
+                    {
+
+                        Console.WriteLine($"Произошла ошибка: {error.Message}");
+
+                    }
+
+                    break;
+
+                }
+
+                else
+                {
+
+                    Console.Write("Указан некорректный путь к папке либо же неверно указано название папки. Проверьте и повторите еще раз: ");
+                    string correctPath = Console.ReadLine();
+                    if (Directory.Exists(correctPath))
+                    {
+                        path = correctPath;
+                        
+                    }
+
+
+                }
+
+
+
+            }
+
+            return size;
+
+        }
+    }
+}
