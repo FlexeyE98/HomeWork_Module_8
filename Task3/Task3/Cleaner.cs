c5d713c764c8da6432462f1330a374f8473e80ed
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-05-11 21:25:02 +0300

    Задание №3

---
+++ Task3/Task3/Cleaner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Cleaner
+    {
+        public DateTime LastAccessTimeUtc { get; set; }
+        public long Size { get; private set; }
+
+        public Cleaner(string path)
+        {
+            LastAccessTimeUtc = Directory.GetLastAccessTimeUtc(path);
+        }
+
+
+        public long CleanFolder(string path)
+        {
+            long size = 0;
+
+            if (Directory.Exists(path)) //&& Directory.GetLastAccessTime(path).Minute > TimeSpan.FromMinutes(30).Minutes)
+            {
+                try
+                {
+                    DirectoryInfo directories = new DirectoryInfo(path);
+                    DirectoryInfo[] dirs = directories.GetDirectories();
+                    FileInfo[] files = directories.GetFiles();
+
+                    foreach (var f in files)
+                    {
+                        size += f.Length;
+                        f.Delete();
+
+                    }
+
+                    foreach (var dir in dirs)
+                    {
+                        size += CleanFolder(dir.FullName);
+                        dir.Delete(true);
+
+                    }
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Произошла ошибка: {error.Message}");
+
+                }
+
+                Size = size;
+               
+            }
+            return size;
+
+        }
+
+
+
+    }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\\Users\Привет\Desktop\test";
+            Sizer sizer = new Sizer();
+            sizer.SizerFolder(path);
+            Console.WriteLine($"Исходный размер папки: {sizer.Size}");
+            Cleaner cleaner = new Cleaner(path);
+            cleaner.CleanFolder(path);
+            Console.WriteLine($"Было освобождено: {cleaner.Size}");
+            long longSizer = sizer.SizerFolder(path);
+            Console.WriteLine($"Текущий размер папки: {longSizer}");
+
+            Console.ReadLine();
+
+            /*
+             
+             Не очень понял как обойти в рекурсии метод Console.WriteLine для вывода информации.
+             Метод при первой итерации выводит размер подпапки и далее с каждой итерацией захода рекурсии входит в следующую и выводит инфу на экран
+             Такой подход немного отличается от того, что требуется в задаче. Поэтому пришлось кривокодить :)
+             
+             */
+
+        }
+    }
+}
