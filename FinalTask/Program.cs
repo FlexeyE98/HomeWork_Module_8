676cfe3b26236a46446f6f45fe9c20701cbe1123
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-05-12 19:36:29 +0300

    Итоговое задание модуля 8

---
+++ FinalTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"C:\Users\Привет\Desktop\Students.dat"; //enter your path to bin.file
+            Student student = new Student();
+            student.SortStudents(path);
+            
+
+
+        }
+    }
+
+
+
+}
---
+++ FinalTask/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Unicode;
+
+namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+
+        public string Name { get; private set; }
+        public string Group { get; private set; }
+        public DateTime Birthday { get; private set; }
+
+        public void SortStudents(string path)
+        {
+            while (true)
+            {
+                if (File.Exists(path))
+                {
+                    Directory.CreateDirectory(@"C:\Users\Привет\Desktop\Students");
+                    BinaryFormatter bf = new BinaryFormatter();
+                    using (FileStream fs = new FileStream(path, FileMode.Open))
+                    {
+                        var newStudent = (Student[])bf.Deserialize(fs);
+                        foreach (var student in newStudent)
+                        {
+                            if (student.Group == student.Group)
+                            {
+                                using (StreamWriter sw = File.AppendText(@$"C:\Users\Привет\Desktop\Students\{student.Group}.txt"))
+                                {
+                                    sw.WriteLine($"Студент: {student.Name}, Дата рождения: {student.Birthday}");
+
+
+                                }
+                                
+                            }
+
+                        }
+
+                        break;
+                    }
+
+                }
+                else
+                {
+                    Console.Write("Такого файла не существует. Повторите попытку: ");
+                    string correctPath = Console.ReadLine();
+                    if (File.Exists(correctPath))
+                    {
+                        path = correctPath;
+
+                    }
+
+                }
+
+
+            }
+
+        }
+    }
+}
